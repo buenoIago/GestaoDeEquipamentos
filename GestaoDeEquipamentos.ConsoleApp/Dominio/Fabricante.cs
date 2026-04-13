75efc47c81f407a72b73c45cd38357ee5a9e65da
buenoIago <iagocedup@gmail.com>
2026-04-13 16:14:35 -0300

    Adicionado Fabricante

---
+++ GestaoDeEquipamentos.ConsoleApp/Dominio/Fabricante.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace GestaoDeEquipamentos.ConsoleApp.Dominio;
+
+/*
+• Deve ter um identificador único (id);
+• Deve ter o nome do fabricante;
+• Deve ter o email do fabricante;
+• Deve ter o telefone do fabricante;
+*/
+
+public class Fabricante
+{
+    public string id;
+    public string nome;
+    public string email;
+    public decimal telefone;
+}
